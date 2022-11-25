6480884073b98e725cca7bd52b2ea9d9b3186d6d
aivaz88 <aivaz1988@mail.ru>
2022-11-25 11:49:46 +0530

    added method for calculate size new array

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+string[] x = {"hello", "2", "world", ":-)"};
+
+
+int SizeNewArray(string[] firsArray)
+{
+    int size = 0;
+    for (int i = 0; i < firsArray.GetLength(0); i++)
+    {
+        if (firsArray[i].Length <= 3) size++;
+    }
+    return size;
+}
+
